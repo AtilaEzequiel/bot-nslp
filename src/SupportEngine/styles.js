66ddfb910bd11a7fb5e407665ca7ad8a778cfad2
AtilaEzequiel <112121260+AtilaEzequiel@users.noreply.github.com>
2022-10-12 15:16:03 -0300

    Revert "front-end pagina"
    This reverts commit e82e0358110e341334646c09558b832879953615.

--- src/SupportEngine/styles.js
+++ src/SupportEngine/styles.js
@@ -1,7 +1,7 @@
 export const styles = {
     chatWithMeButton: {
         cursor: 'pointer',
-        boxShadow: '0px 0px 16px 6px green',
+        boxShadow: '0px 0px 16px 6px rgba(0, 0, 0, 0.23)',
         // Border
         borderRadius: '50%',
         // Background 
@@ -9,7 +9,7 @@ export const styles = {
         backgroundRepeat: 'no-repeat',
         backgroundPosition: 'center',
         backgroundSize: '30px',
-        backgroundColor: '#f8cea2',
+        backgroundColor: '#9f964a',
         // Size
         width: '75px',
         height: '75px',
@@ -26,11 +26,10 @@ export const styles = {
         padding: '12px 12px 12px 16px',
         borderRadius: '24px', 
         // Color
-        backgroundColor: '#f8cea2',
+        backgroundColor: '#9f964a',
         color: 'black',
     },
     supportWindow: {
-        
         // Position
         position: 'fixed',
         bottom: '116px',
@@ -40,13 +39,13 @@ export const styles = {
         height: '530px',
         maxWidth: 'calc(100% - 48px)',
         maxHeight: 'calc(100% - 48px)',
-        backgroundColor: 'black',
+        backgroundColor: 'white',
         // Border
         borderRadius: '12px',
-        border: `2px solid #f8cea2`,
+        border: `2px solid #9f964a`,
         overflow: 'hidden',
         // Shadow
-        boxShadow: '0px 0px 16px 6px green',
+        boxShadow: '0px 0px 16px 6px rgba(0, 0, 0, 0.33)',
     },
     emailFormWindow: { 
         width: '100%',  
@@ -60,14 +59,14 @@ export const styles = {
         top: '-45px',
         width: '100%',
         height: '308px',
-        backgroundColor: '#f8cea2',
+        backgroundColor: '#9f964a',
         transform: 'skewY(-12deg)',
     },
     topText: { 
         position: 'relative',
         width: '100%', 
         top: '15%', 
-        color: 'black', 
+        color: 'white', 
         fontSize: '24px', 
         fontWeight: '600',
     },
@@ -77,13 +76,13 @@ export const styles = {
         outline: 'none',
         padding: '12px',
         borderRadius: '12px',
-        border: '2px solid #f8cea2',
+        border: '2px solid #9f964a',
     },
     bottomText: { 
         position: 'absolute', 
         width: '100%', 
         top: '60%', 
-        color: '#f8cea2', 
+        color: '#9f964a', 
         fontSize: '24px', 
         fontWeight: '600' 
     },
@@ -95,7 +94,7 @@ export const styles = {
         backgroundColor: 'white',
     },
     loadingIcon: { 
-        color: '#f8cea2', 
+        color: '#9f964a', 
         position: 'absolute', 
         top: 'calc(50% - 51px)', 
         left: 'calc(50% - 51px)',  
